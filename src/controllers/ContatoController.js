ca23a68e6353bf38c2ca906f44a088e3f14a0119
vitor7001 <vitorgomes7001@gmail.com>
2020-05-13 20:19:06 -0300

    Rota para buscar um contato em especifico e status de retorno

--- src/controllers/ContatoController.js
+++ src/controllers/ContatoController.js
@@ -1,15 +1,38 @@
 const knex = require('../database/conexao')
 
+
+
 module.exports = {
     //metodo que retorna todos os contatos da tabela
     async index(req, res){
 
-    const results = await knex('contatos').orderBy('id')
+        try {
 
-        if( !results ){
-            return res.status(204).send()
-        }else{
-            return res.json(results)
+        const results = await knex('contatos').orderBy('id')
+
+        return res.status(200).json(results)
+
+            
+        } catch (error) {
+            const erro = new Error('Falha ao buscar contatos')
+            erro.status = 404
+            next(erro)
+        }
+
+
+    },
+
+    //metodo que busca um contato pelo id
+    async getById(req, res){
+
+        try {
+            const {id} = req.params
+            const results = await knex('contatos').where({id})
+            return res.status(200).json(results)
+        } catch (error) {
+            const erro = new Error('Falha ao buscar contato')
+            erro.status = 404
+            next(erro)
         }
 
     },
@@ -18,7 +41,6 @@ module.exports = {
     async create(req, res, next){
 
         try {
-
             const { nome } = req.body
             const { email } = req.body
             const { telefone } = req.body
@@ -28,13 +50,16 @@ module.exports = {
 
             return res.status(201).send()
         } catch (error) {
-            next(error)
+            const erro = new Error('Falha ao inserir contato')
+            erro.status = 400
+            next(erro)
         }
     },
 
     //metodo que atualiza um contato
     async update(req,res,next){
-        try {
+        try {   
+
             const { id } = req.params
             const { nome } = req.body
             const { email } = req.body
@@ -45,26 +70,29 @@ module.exports = {
             await knex('contatos').update({ nome, email, telefone, data_nascimento, endereco })
             .where({id})
 
-            return res.send()
+            return res.status(204).send() 
         } catch (error) {
-            next(error)
+            const erro = new Error('Falha ao inserir contato')
+            erro.status = 400
+            next(erro)
         }
     },
 
     //metodo que deleta um contato
     async delete(req,res,next){
-
         try {
-            
             const {id} = req.params
-             await knex('contatos')
+
+            await knex('contatos')
             .where({id})
             .del()
 
-            return res.status(204).send()
+            return res.status(204).send()         
             
         } catch (error) {
-            next(error)
+            const erro = new Error('Falha ao deletar contato')
+            erro.status = 400
+            next(erro)
         }
 
     }
